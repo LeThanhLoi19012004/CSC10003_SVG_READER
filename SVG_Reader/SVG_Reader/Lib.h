72a4e4fdde23990af81d8963ba7251313f2c9266
LeThanhLoi19012004 <ltloi22@clc.fitus.edu.vn>
2023-12-03 16:37:48 +0700

    GDI+ (main)

--- SVG_Reader/SVG_Reader/Lib.h
+++ SVG_Reader/SVG_Reader/Lib.h
@@ -1,8 +1,8 @@
 #ifndef _LIB_H_
 #define _LIB_H_
 
-#define MAX 255
-#define Pi 3.1416
+//#define MAX 255
+//#define Pi 3.1416
 
 #include <iostream>
 #include <vector>
@@ -11,26 +11,34 @@
 #include <sstream>
 #include <cmath>
 #include <unordered_map>
+#include <stack>
 
-#include <SFML/Graphics.hpp>
-#include <SFML/Window/KeyBoard.hpp>
-
+#include "framework.h"
+#include "rapidxml.hpp"
+#include <windows.h>
+#include <objidl.h>
+#include <gdiplus.h>
 using namespace std;
+using namespace rapidxml;
+using namespace Gdiplus;
+#pragma comment (lib,"Gdiplus.lib")
 
-#include "ColorProperty.h"
-#include "Figure.h"
-#include "Point.h"
-#include "Polygon.h"
-#include "Rectangle.h"
-#include "Ellipse.h"
-#include "Circle.h"
-#include "Polyline.h"
-#include "Line.h"
-#include "Text.h"
-#include "FactoryFigure.h"
-#include "Parser.h"
-#include "Renderer.h"
-#include "Image.h"
-#include "Program.h"
+//#include "ColorProperty.h"
+//#include "Figure.h"
+//#include "Group.h"
+//#include "Point.h"
+//#include "Polygon.h"
+//#include "Rectangle.h"
+//#include "Ellipse.h"
+//#include "Circle.h"
+//#include "Polyline.h"
+//#include "Line.h"
+//#include "Text.h"
+//#include "Path.h"
+//#include "FactoryFigure.h"
+//#include "Parser.h"
+//#include "Renderer.h"
+//#include "Image.h"
+//#include "Program.h"
 
 #endif 
